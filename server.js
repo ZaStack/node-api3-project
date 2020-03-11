007cacd800b85da2f8360f8f424fb5c4dfc7a4c5
ZaStack <stackzpersonal@gmail.com>
2020-03-11 14:46:54 -0700

    Working all the way up through delete

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const server = express();
 // const logger = require(logger)
-
+const userRouter = require('./users/userRouter')
 const postRouter = require('./posts/postRouter')
 
-server.use('/api/posts', logger, postRouter)
+server.use(express.json())
+server.use('/api/users', logger, userRouter)
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,90 @@
 const express = require('express');
-
 const router = express.Router();
+const postRouter = require('../posts/postRouter');
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
 
 router.post('/', (req, res) => {
-  // do your magic!
+    const user = req.body;
+    Users.insert(user)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+    const { body } = req;
+    Posts.insert(body)
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+    Users.get(req.query)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
+    Users.getById(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
+    Users.getUserPosts(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
+    Users.remove(id)
+        .then(user => {
+          res.status(200).json(user)
+        })
+        .catch(err => {
+          console.log(`Error: ${err}`)
+        });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+    // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+    // do your magic!
 }
 
 module.exports = router;
