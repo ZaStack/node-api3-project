c5cc66694919119d6972105a02f50a8181ebf766
ZaStack <stackzpersonal@gmail.com>
2020-03-11 13:18:40 -0700

    Logger

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
 const server = express();
+// const logger = require(logger)
 
-server.get('/', (req, res) => {
+const postRouter = require('./posts/postRouter')
+
+server.use('/api/posts', logger, postRouter)
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+  const time = new Date();
+
+  console.log(`${method} to ${endpoint} at ${time}`);
+  next();
+}
 
 module.exports = server;
